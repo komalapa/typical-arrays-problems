e836e4c6eec13f3b04b6a7d57c7e5ec255088f49
komalapa <komalapa@gmail.com>
2021-07-19 18:52:51 +0400

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!array || array.length === 0) return 0;
+    let min = 0;
+    array.forEach(element => {
+         if (element < min){
+             min = element;
+         }     
+    });
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!array || array.length === 0) return 0;
+    let max = 0;
+    array.forEach(element => {
+         if (element > max){
+             max = element;
+         }     
+    });
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!array || array.length === 0) return 0;
+    let avg = array.reduce((sum,e) => sum+e)/array.length
+    return avg;
 }
